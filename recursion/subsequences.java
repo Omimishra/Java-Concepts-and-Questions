458badaf4da5e523dcbfc9b2ea98f2ae842a0cbb
Omi Mishra <work.omimishra@gmail.com>
2024-09-29 21:36:33 +0530

    #23 Recursion : Printing subsequences

---
+++ recursion/subsequences.java
@@ -0,0 +1,23 @@
+package recursion;
+
+public class subsequences {
+    public static void subs(String str, int i, String str1) {
+        if (i == str.length()) {
+            System.out.println(str1);
+            return;
+        }
+        char currchar = str.charAt(i);
+
+        // choice of char to be in string
+        subs(str, i + 1, str1 + currchar);
+
+        // choice of char to be in string
+        subs(str, i + 1, str1);
+    }
+
+    public static void main(String[] args) {
+        String str = "aaa";
+        subs(str, 0, " ");
+    }
+}
+// time complexity of this is 2^n
