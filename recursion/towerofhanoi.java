0449b8b0ee00270e8a59c6aff1d0a685589bad09
Omi Mishra <work.omimishra@gmail.com>
2024-10-04 21:27:17 +0530

    #31 Recursion: Tower of hanoi

---
+++ recursion/towerofhanoi.java
@@ -0,0 +1,18 @@
+package recursion;
+
+public class towerofhanoi {
+    public static void toh(int n, String src, String help, String dest) {
+        if (n == 1) {
+            System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+            return;
+        }
+        toh(n - 1, src, dest, help);
+        System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+        toh(n - 1, help, src, dest);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        toh(n, "S", "H", "D");
+    }
+}
