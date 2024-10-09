9a7b1c086ed8b3b327bbc7036c34c878d7fae535
Omi Mishra <work.omimishra@gmail.com>
2024-10-09 17:45:20 +0530

    modified merge sort

--- arrays/mergesort.java
+++ arrays/mergesort.java
@@ -1,44 +1,59 @@
 package arrays;
 public class mergesort {
-    public static void divide(int arr[], int si, int ei) {
+    public static void divide(int arr[], int st, int end) {
         // o(log n)
-        if (si >= ei) {
+        if (st >= end) {
             return;
         }
-        int mid = si + (ei - si) / 2;
-        divide(arr, si, mid);
-        divide(arr, mid + 1, ei);
-        conquer(arr, si, mid, ei);
+        int mid = st + (end - st) / 2;
+        divide(arr, st, mid);
+        divide(arr, mid + 1, end);
+        conquer(arr, st, mid, end);
     }
 
-    public static void conquer(int arr[], int si, int mid, int ei) {
+    public static void conquer(int arr[], int st, int mid, int end) {
         // o(n)
-        int merged[] = new int[ei - si + 1];
-        int idx1 = si;
-        int idx2 = mid + 1;
-        int x = 0;
-        while (idx1 <= mid && idx2 <= ei) {
-            if (arr[idx1] <= arr[idx2]) {
-                merged[x] = arr[idx1];
-                x++;
-                idx1++;
-            } else {
-                merged[x++] = arr[idx2++];
-
+        int n1 = mid-st+1;
+        int n2 = end-mid;
+        
+        int left[] = new int[n1];
+        int right[] = new int[n2];
+        
+        for (int i = 0 ; i<n1;i++){
+            left[i] = arr[st+i];
+        }
+        for (int i = 0 ; i<n2;i++){
+            right[i] = arr[mid+i+1];
+        }
+        
+        int i = 0; 
+        int j= 0 ; 
+        int k = st;
+        
+        while(i<n1 && j<n2){
+            if(left[i] <= right[j]){
+                arr[k] = left[i];
+                i++;
+            }
+            else
+            {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+            
+            while(i<n1){
+                arr[k] = left[i];
+                i++;
+                k++;
+            }
+            while(j<n2){
+                arr[k] = right[j];
+                j++;
+                k++;
             }
         }
-        while (idx1 <= mid) {
-            merged[x] = arr[idx1];
-            x++;
-            idx1++;
-        }
-        while (idx2 <= ei) {
-            merged[x++] = arr[idx2++];
-        }
-        for (int i = 0, j = si; i < merged.length; i++, j++) {
-            arr[j] = merged[i];
-        }
-    }
 
     public static void main(String[] args) {
         int arr[] = { 6, 8, 3, 5, 1, 3, 4, 1, 9 };
