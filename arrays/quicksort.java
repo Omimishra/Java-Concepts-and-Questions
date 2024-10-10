1a09add305da87eea6f9931ccdc44530ede5c891
Omi Mishra <work.omimishra@gmail.com>
2024-10-10 19:32:18 +0530

    modified quick sort

--- arrays/quicksort.java
+++ arrays/quicksort.java
@@ -3,22 +3,28 @@ public class quicksort {
     //worst case o(n^2)
     //average case o(nlogn)
     public static int partition(int arr[], int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while(i<j) {
+            while(arr[i] <= pivot && i<=high-1) {
                 i++;
-                // swap
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
             }
+            while(arr[j] > pivot && j>=low+1) {
+                j--;
+            }
+            if(i<j)
+            {
+            // swap
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
-        i++;
-        int temp = arr[i];
-        arr[i] = arr[high];
-        arr[high] = temp;
-        return i;// pivot index
+    }
+        int temp = arr[j];
+        arr[j] = arr[low];
+        arr[low] = temp;
+        return j;// pivot index
     }
 
     public static void sort(int arr[], int low, int high) {
