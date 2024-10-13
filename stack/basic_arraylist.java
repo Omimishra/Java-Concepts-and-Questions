1fd073fda273b1a0a4629522268cc4b4db0f5177
Omi Mishra <work.omimishra@gmail.com>
2024-10-13 19:03:17 +0530

    #44 Stack : Pushing element at bottom

--- stack/basic_arraylist.java
+++ stack/basic_arraylist.java
@@ -6,32 +6,36 @@ import stack.basic_ll.stack;
 
 public class basic_arraylist {
     static class Stack {
-       static  ArrayList<Integer> list = new ArrayList<>();
-        public static boolean isempty(){
-            return list.size() ==0;
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isempty() {
+            return list.size() == 0;
         }
-public static void push(int data){
-    list.add(data);
 
-}
-public static int pop(){
-    if (isempty()){
-        return -1;
-    }
-    int top = list.get(list.size()-1);
-    list.remove(list.size()-1);
-    return top;
-}
-public static int peek(){
-    if (isempty()){
-        return -1;
-    }
-    return list.get(list.size()-1);
-}
+        public static void push(int data) {
+            list.add(data);
+
+        }
+
+        public static int pop() {
+            if (isempty()) {
+                return -1;
+            }
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        public static int peek() {
+            if (isempty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
     }
 
- @SuppressWarnings("unused")
-public static void main(String[] args) {
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
         stack s = new stack();
         stack.push(0);
         stack.push(1);
@@ -44,5 +48,5 @@ public static void main(String[] args) {
             stack.pop();
 
         }
-}
+    }
 }
