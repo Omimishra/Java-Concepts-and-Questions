1c6688b1c4cb1663c8904221018ca1cb614b98d2
Omi Mishra <work.omimishra@gmail.com>
2025-01-12 17:07:39 +0530

    #84 Arrays: Count maximum consecutive 1's in an array

---
+++ arrays/Count_max_1.java
@@ -0,0 +1,23 @@
+package arrays;
+public class Count_max_1 {
+
+    public static int count_max_1(int[] a) {
+        int n = a.length;
+        int max_1 = 0;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (a[i] == 1) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            max_1 = Math.max(max_1, cnt);
+        }
+        return max_1;
+    }
+    public static void main(String[] args) {
+        int [] a = {1, 1, 0, 1, 0, 1, 0, 0, 1};
+    int result = count_max_1(a);
+    System.out.println(result);
+}
+}
