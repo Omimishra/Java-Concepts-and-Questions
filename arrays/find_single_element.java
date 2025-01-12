b8a299fda95d6dfee72aeb42e329fe553e5b7959
Omi Mishra <work.omimishra@gmail.com>
2025-01-12 17:23:49 +0530

    #85 Array: Find a element which appears only once in the array

---
+++ arrays/find_single_element.java
@@ -0,0 +1,22 @@
+package arrays;
+import java.util.HashMap;
+public class find_single_element {
+    public static int brute_force(int[] a) {
+        int n = a.length;
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            map.put(a[i], map.getOrDefault(a[i], 0) + 1);
+        }
+        for (int i =0;i<n;i++){
+            if(map.get(a[i])==1){
+                return a[i];
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+int a[] = {1, 2, 3, 4, 5, 1, 2, 3, 4};
+System.out.println(brute_force(a));
+    }
+}
