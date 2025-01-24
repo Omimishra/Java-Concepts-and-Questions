f30ca18ef3360b8b44e6f721b40b298ad9af70fa
Omi Mishra <work.omimishra@gmail.com>
2025-01-24 22:57:18 +0530

    #93 Optimized solution of previous question

---
+++ arrays/set_0_optimized.java
@@ -0,0 +1,71 @@
+package arrays;
+public class set_0_optimized {
+    static int[] [] zeroMatrix(int matrix [][], int n, int m) {
+        // int[] row = new int[n]; --> matrix[..][0]
+        // int[] col = new int[m]; --> matrix[0][..]
+
+        int col0 = 1;
+        // step 1: Traverse the matrix and
+        // mark 1st row & col accordingly:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    // mark i-th row:
+                    matrix[i][0] = 0;
+
+                    // mark j-th column:
+                    if (j != 0)
+                        matrix[0][j]=0;
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != 0) {
+                    // check for col & row:
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        //step 3: Finally mark the 1st col & then 1st row:
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        int A[][] = {
+            {1, 0, 1},
+            {1, 1, 1},
+            {1, 1, 1}
+        };
+        int n = A.length;
+        int m = A[0].length;
+        int ans[][] = zeroMatrix(A, n, m);
+        for(int i = 0; i < ans.length; i++) {
+            for(int j = 0; j < ans[0].length; j++) {
+                System.out.print(ans[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
