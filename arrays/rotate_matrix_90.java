904519c9096ebb3f5ca404f1cb654d53b5e60329
Omi Mishra <work.omimishra@gmail.com>
2025-01-25 19:22:32 +0530

    # Made last code more optimized

--- arrays/rotate_matrix_90.java
+++ arrays/rotate_matrix_90.java
@@ -4,20 +4,42 @@ public class rotate_matrix_90 {
     public static int[][] rotate(int[][] matrix) {
         int n = matrix.length;
         int result[][] = new int[n][n];
-        for(int i =0;i<n;i++){
-            for(int j =0;j<n;j++){
-                result[j][n-i-1] = matrix[i][j];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                result[j][n - i - 1] = matrix[i][j];
             }
         }
-    return result;
+        return result;
+    }
+
+    public static void optimized(int[][] matrix) {
+        // Transpose the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // Reverse the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - j - 1];
+                matrix[i][matrix.length - j - 1] = temp;
+            }
+        }
+
     }
 
     public static void main(String[] args) {
-        int matrix[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         int result[][] = rotate(matrix);
-        for(int i =0;i<result.length;i++){
-            for(int j =0;j<result.length;j++){
-                System.out.print(result[i][j]+" ");
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result.length; j++) {
+                System.out.print(result[i][j] + " ");
             }
             System.out.println();
         }
