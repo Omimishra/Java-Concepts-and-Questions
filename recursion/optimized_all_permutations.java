fe40fa32e26a1c93dd0e27ddaaab9061bea37557
Omi Mishra <work.omimishra@gmail.com>
2025-01-29 17:01:27 +0530

    #98 Recursion:All permutations

---
+++ recursion/optimized_all_permutations.java
@@ -0,0 +1,28 @@
+package recursion;
+
+public class optimized_all_permutations {
+    public void permute(int[] nums, int index) {
+        if (index == nums.length) {
+            for (int i = 0; i < nums.length; i++) {
+                System.out.print(nums[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            swap(nums, i, index);//swap the elements
+            permute(nums, index + 1);//
+            swap(nums, i, index);
+        }
+    }
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3 };
+        optimized_all_permutations obj = new optimized_all_permutations();
+        obj.permute(nums, 0);
+    }
+}
---
+++ recursion/permutations.java
@@ -0,0 +1,30 @@
+package recursion;
+import java.util.ArrayList;
+public class permutations {
+    public static ArrayList<ArrayList<Integer>> permute(int[] nums) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> tempList = new ArrayList<>();
+        backtrack(res, tempList, nums);
+        return res;
+    }
+    public static void backtrack(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> tempList, int[] nums) {
+        if (tempList.size() == nums.length) {//if tempList is equal to the length of the array then add it to the result it means we have found one permutation
+            res.add(new ArrayList<>(tempList));
+            return;
+        } 
+            for (int i = 0; i < nums.length; i++) {
+                if (!tempList.contains(nums[i])) {//if the tempList does not contain the element then add it to the tempList and backtrack
+                    tempList.add(nums[i]);
+                    backtrack(res, tempList, nums);
+                    tempList.remove(tempList.size() - 1);//remove the last element from the tempList
+                }
+            }
+        }
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3 };
+        ArrayList<ArrayList<Integer>> res = permute(nums);
+        System.out.println(res);
+    }
+
+}
