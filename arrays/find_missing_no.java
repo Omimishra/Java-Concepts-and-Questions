4f8a4e859b0a8e961ebe5ff1b3ce193178d41a38
Omi Mishra <work.omimishra@gmail.com>
2025-02-03 23:10:24 +0530

    #used xor approach for previous problem

--- arrays/find_missing_no.java
+++ arrays/find_missing_no.java
@@ -10,8 +10,19 @@ public class find_missing_no {
         }
         return sum-s2;
     }
+    public static int xor(int a[]){ // xor method
+        int n = a.length;
+        int x1=0;
+        int x2=0;
+        for(int i =0;i<n;i++){
+            x2^=a[i];
+            x1^=(i+1);
+        }
+        return x1^x2;
+    }
     public static void main(String[] args) {
         int a[] = {9,6,4,2,3,5,7,0,1};
-        System.out.println(find(a));
+        // System.out.println(find(a));
+        System.out.println(xor(a));
     }
 }
