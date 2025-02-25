6af15ac29eddffe2d28d41fb8bfe479c771d16f2
Omi Mishra <work.omimishra@gmail.com>
2025-02-25 22:36:03 +0530

    #115 Basics of linkedlist

---
+++ linked list/basic.java
@@ -0,0 +1,53 @@
+public class basic {
+    node head;
+    static class node {
+        int data;
+        node next;
+
+        public node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+
+    }
+
+    static node constructLL(int arr[]) {
+        if (arr.length == 0) {
+            return null;
+        }
+        node head = new node(arr[0]);
+        node temp = head;
+        for (int i = 1; i < arr.length; i++) {
+            temp.next = new node(arr[i]);
+            temp = temp.next;
+        }
+        return head;
+    }
+
+    public node insertAtFirst(int data) {
+        node newNode = new node(data);
+        if(head==null){
+            head=newNode;
+            return head;
+        }
+        newNode.next = head;
+        head=newNode;
+        return head;
+    }
+    
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        // for(int i =0;i<arr.length;i++){
+        // node n = new node(arr[i]);
+        // System.out.print(n.data+"->");
+        // }
+        node head = constructLL(arr);
+        node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + "->");
+            curr = curr.next;
+        }
+        System.out.println("null");
+    }
+}
