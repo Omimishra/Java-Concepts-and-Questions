f0a50ad12d1e85ffda76ab31fa94056bc0faeb35
Omi Mishra <work.omimishra@gmail.com>
2025-03-17 22:06:49 +0530

    #132 Find nth node from the end

--- linked list/Find_nth_node_from_end.java
+++ linked list/Find_nth_node_from_end.java
@@ -1,89 +1,63 @@
 // import java.util.*;
 //Find the nth node from the end and remove it.
+class Node {
+    int data;
+    Node next;
+    
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+        
+    }
+}
 public class Find_nth_node_from_end {
-    Node head;
-
-    class Node {
-        int data;
-        Node next;
-
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-
+    // Function to print the linked list
+    public static void printLL(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
         }
     }
 
-    public void addLast(int data) {
-        Node newnode = new Node(data);
+    // Function to delete the Nth node from the end of the linked list
+    public static Node DeleteNthNodefromEnd(Node head, int N) {
+        // Create two pointers, fastp and slowp
+        Node fastp = head;
+        Node slowp = head;
 
-        if (head == null) {
-            head = newnode;
-        }
-        Node currNode = head;
-        while (currNode.next != null) {
-            currNode = currNode.next;
-        }
-        currNode.next = newnode;
+        // Move the fastp pointer N nodes ahead
+        for (int i = 0; i < N; i++)
+            fastp = fastp.next;
 
-    }
-
-    public void printlist() {
-
-        if (head == null) {
-            System.out.println("List is empty");
-            return;
-        }
-
-        Node currNode = head;
-        while (currNode != null) {
-            System.out.print(currNode.data + "->");
-            currNode = currNode.next;
-        }
-
-        System.out.println("NULL");
-
-    }
-
-    public Node removeNthNode(Node head, int n) {
-        if (head.next == null) {
-            return null;
-
-        }
-        int size = 0;
-        Node currNode = head;
-        while (currNode != null) {
-            currNode = currNode.next;
-            size++;
-        }
-        // removing SIZEth node from last i.e head
-        if (n == size) {
+        // If fastp becomes null, the Nth node from the end is the head
+        if (fastp == null)
             return head.next;
+
+        // Move both pointers until fastp reaches the end
+        while (fastp.next != null) {
+            fastp = fastp.next;
+            slowp = slowp.next;
         }
 
-        // find previous node 
-        int indexToSearch = size - n; // position to find 
-        Node prev = head; // previous node
-        int i = 1;
-        while (i < indexToSearch) { // iterate to find the true position of previous node
-            prev = prev.next;
-            i++;
-        }
-        prev.next = prev.next.next;
+        // Delete the Nth node from the end
+        @SuppressWarnings("unused")
+        Node delNode = slowp.next;
+        slowp.next = slowp.next.next;
+        delNode = null;
         return head;
     }
 
     public static void main(String[] args) {
-        Find_nth_node_from_end list = new Find_nth_node_from_end();
-        list.addLast(1);
-        list.addLast(2);
-        list.addLast(3);
-        list.addLast(4);
-        list.addLast(5);
-        System.out.println("Enter the Nth node from the end of the list");
-        // Scanner sc = new Scanner(System.in);
-        // int n = sc.nextInt();
-        list.head = list.removeNthNode(list.head, 3);
-        list.printlist();
+        int[] arr = {1, 2, 3, 4, 5};
+        int N = 3;
+        Node head = new Node(arr[0]);
+        head.next = new Node(arr[1]);
+        head.next.next = new Node(arr[2]);
+        head.next.next.next = new Node(arr[3]);
+        head.next.next.next.next = new Node(arr[4]);
+
+        // Delete the Nth node from the end and print the modified linked list
+        head = DeleteNthNodefromEnd(head, N);
+        printLL(head);
     }
 }
