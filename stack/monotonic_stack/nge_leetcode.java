68ce9e2a2b8dcc334419dc1d927e29fb5bb9f8e5
Omi Mishra <work.omimishra@gmail.com>
2025-04-25 22:52:17 +0530

    #modified code

--- stack/monotonic_stack/nge_leetcode.java
+++ stack/monotonic_stack/nge_leetcode.java
@@ -5,14 +5,22 @@ import java.util.*;
 // Output: [-1,3,-1]
 public class nge_leetcode {
     public int[] nextGreaterElement(int[] nums1, int[] nums2){
+        // nums1 is the array for which we need to find the next greater element
+        // nums2 is the array in which we need to find the next greater element
         Map<Integer,Integer> map = new HashMap<>();
         Stack<Integer> st = new Stack<>();
         for (int num:nums2) {
+            // if stack is not empty and the current number is greater than the top of the stack
+            // then we pop the stack and put the current number in the map with the popped number as key
+
             while (!st.isEmpty() && num >= st.peek())
                map.put(st.pop(),num);
             st.push(num);
         }
         for(int i =0;i<nums1.length;i++){
+            // if the number is not in the map then we put -1 in the map for that number
+            // else we put the value of the number in the map for that number
+            // so that we can get the next greater element for that number in O(1) time
             nums1[i]=map.getOrDefault(nums1[i],-1);
         }
         return nums1;
