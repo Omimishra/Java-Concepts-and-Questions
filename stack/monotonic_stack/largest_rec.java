c21d384481d65a0915e9f55c0ba81421610e8ab9
Omi Mishra <work.omimishra@gmail.com>
2025-05-02 19:37:47 +0530

    #modified previous code

--- stack/monotonic_stack/largest_rec.java
+++ stack/monotonic_stack/largest_rec.java
@@ -6,24 +6,29 @@ public class largest_rec {
         Stack < Integer > st = new Stack < > ();
         int maxA = 0;
         int n = histo.length;
-        for (int i = 0; i <= n; i++) {
-            while (!st.empty() && (i == n || histo[st.peek()] >= histo[i])) {
-                int height = histo[st.peek()];
+        for (int i = 0; i <n; i++) {
+            while (!st.empty() && (histo[st.peek()] >= histo[i])) {
+                int element = histo[st.peek()];
                 st.pop();
-                int width;
-                if (st.empty())
-                    width = i;
-                else
-                    width = i - st.peek() - 1;
-                maxA = Math.max(maxA, width * height);
+                int nse = i;
+                int pse = st.empty() ? -1 : st.peek();
+                maxA = Math.max(maxA, element * (nse - pse - 1));
             }
             st.push(i);
+        }
+        while (!st.empty()) {
+            int element = histo[st.peek()];
+            st.pop();
+            int nse = n;
+            int pse = st.empty() ? -1 : st.peek();
+            maxA = Math.max(maxA, element * (nse - pse - 1));
+            
         }
         return maxA;
     }
 
     public static void main(String args[]) {
-        int histo[] = {3, 1, 5, 6, 2, 3};
+        int histo[] = {2,1,5,6,2,3};
         System.out.println("The largest area in the histogram is " + largestRectangleArea(histo));
     }
 }
