cdc177f26b6974bca9e0da806b40fbfefea210b5
Omi Mishra <work.omimishra@gmail.com>
2025-05-07 21:22:18 +0530

    #166 Online stock span

---
+++ stack/online_stock_span.java
@@ -0,0 +1,29 @@
+package stack;
+import java.util.Stack;
+public class online_stock_span {
+    Stack<int[]>st;
+    int i = 0;
+    public online_stock_span() {
+        st = new Stack<>();
+    }
+    
+    public int next(int price) {
+        while(!st.isEmpty() && st.peek()[0] <= price){
+            st.pop();
+        }
+        i++;
+        int x = i- (st.isEmpty() ? 0 : st.peek()[1]);
+        st.push(new int[]{price,i});
+        return x;
+    }
+    public static void main(String[] args) {
+        online_stock_span obj = new online_stock_span();
+        System.out.println(obj.next(100)); // 1
+        System.out.println(obj.next(80));  // 1
+        System.out.println(obj.next(60));  // 1
+        System.out.println(obj.next(70));  // 2
+        System.out.println(obj.next(60));  // 1
+        System.out.println(obj.next(75));  // 4
+        System.out.println(obj.next(85));  // 6
+    }
+}
