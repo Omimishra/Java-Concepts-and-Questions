7484b1796486c65e994d0e9c06743ec3dfce3e75
Omi Mishra <work.omimishra@gmail.com>
2025-05-10 22:53:31 +0530

    #modified some codes

--- stack/learning/check_balanced_brackets.java
+++ stack/learning/check_balanced_brackets.java
@@ -8,18 +8,24 @@ public class check_balanced_brackets {
         for (int i = 0; i < str.length(); i++) {
             char it = str.charAt(i);
             if (it == '{' || it == '[' || it == '(') {
+                // push the opening brackets onto the stack
                 s.push(it);
             } else {
                 if (s.isEmpty())
+                // if the stack is empty and we encounter a closing bracket, return false
                     return false;
                 char ch = s.pop();
+                // check if the popped character matches the current closing bracket
+                // if it does, continue to the next iteration
                 if ((ch == '{' && it == '}') || (ch == '[' && it == ']') || (ch == '(' && it == ')'))
                     continue;
                 else
+                // if it doesn't match, return false
                     return false;
             }
         }
         if (s.isEmpty())
+        // if the stack is empty after processing all characters, return true
             return true;
         else
             return false;
