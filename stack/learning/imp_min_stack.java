bb65e7a8f1e680b5956e82044711533d3b20b8c5
Omi Mishra <work.omimishra@gmail.com>
2025-05-11 22:24:10 +0530

    #modification

--- stack/learning/imp_min_stack.java
+++ stack/learning/imp_min_stack.java
@@ -1,7 +1,9 @@
 package stack.learning;
 import java.util.Stack;
+// Implement a stack that supports push, pop, top, and retrieving the minimum element in constant time.
     class Pair {
         int x, y;
+        // Constructor to initialize the Pair
         Pair(int x, int y) {
             this.x = x;
             this.y = y;
@@ -9,17 +11,22 @@ import java.util.Stack;
     }
     class MinStack {
         Stack < Pair > st;
+        // Constructor to initialize the stack
         public MinStack() {
             st = new Stack < > ();
         }
     
         public void push(int x) {
+            // create a new Pair with the current value and the minimum value
             int min;
             if (st.isEmpty()) {
+                // if the stack is empty, set the minimum value to the current value
                 min = x;
             } else {
+                // if the stack is not empty, set the minimum value to the minimum of the current value and the top of the stack
                 min = Math.min(st.peek().y, x);
             }
+            // push the new Pair onto the stack
             st.push(new Pair(x, min));
         }
     
