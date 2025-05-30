6018a1757946c94ad2dfb04dccfe75cd6f0c0a2d
Omi Mishra <work.omimishra@gmail.com>
2025-05-30 11:48:08 +0530

    #177 String: Checking if one string is a rotation of another

---
+++ strings/rotate_string.java
@@ -0,0 +1,22 @@
+package strings;
+
+public class rotate_string {
+    public static boolean checkRotate(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        String combined = s + s;
+        return combined.contains(t);
+    }
+    public static void main(String[] args) {
+        String s = "abcde";
+        String t = "cdeab";
+        boolean result = checkRotate(s, t);
+        System.out.println(result); // Output: true
+
+        String s2 = "abcde";
+        String t2 = "abced";
+        boolean result2 = checkRotate(s2, t2);
+        System.out.println(result2); // Output: false
+    }
+}
