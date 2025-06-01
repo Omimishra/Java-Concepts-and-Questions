b07df71c5643754838ec484a428207d22bcf0080
Omi Mishra <work.omimishra@gmail.com>
2025-06-01 22:20:58 +0530

    #178 String: Sort characters by their frequency in the given string

---
+++ strings/sort_char_by_freq.java
@@ -0,0 +1,35 @@
+package strings;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+public class sort_char_by_freq {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> frequencyMap = new HashMap<>();
+        // Count the frequency of each character
+        for (char c : s.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+        
+       //build a max heap
+       PriorityQueue<Map.Entry<Character,Integer>> pq = new PriorityQueue<>((a,b)-> b.getValue()-a.getValue());
+       pq.addAll(frequencyMap.entrySet());
+
+       StringBuilder result = new StringBuilder();
+       while(!pq.isEmpty()){
+        Map.Entry<Character,Integer> entry = pq.poll();
+        for(int i =0;i<entry.getValue();i++){
+            result.append(entry.getKey());
+        }
+        }
+        return result.toString();
+
+
+    }
+    public static void main(String[] args) {
+        sort_char_by_freq obj = new sort_char_by_freq();
+        String s = "tree";
+        String result = obj.frequencySort(s);
+        System.out.println(result); // Output: "eetr" or "eert" depending on the implementation
+    }
+}
+
