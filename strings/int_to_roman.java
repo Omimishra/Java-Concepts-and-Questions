584ccefaa1298d14e879a9d3016be171574b800a
Omi Mishra <work.omimishra@gmail.com>
2025-06-07 22:21:14 +0530

    #181 String: Int to roman numerals

---
+++ strings/int_to_roman.java
@@ -0,0 +1,24 @@
+package strings;
+
+public class int_to_roman {
+    public String intToRoman(int num) {
+         int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i=0;i<values.length;i++) {
+        while(num >= values[i]) {
+            num -= values[i];
+            sb.append(strs[i]);
+        }
+    }
+    return sb.toString();
+}
+    public static void main(String[] args) {
+        int_to_roman obj = new int_to_roman();
+        int num = 1994;
+        String result = obj.intToRoman(num);
+        System.out.println(result); // Output: "MCMXCIV"
+    }
+}
