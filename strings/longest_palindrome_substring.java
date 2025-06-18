001e2044a0b934d00be89f0d3638217145b4fda5
Omi Mishra <work.omimishra@gmail.com>
2025-06-18 15:30:52 +0530

    modified recent code

--- strings/longest_palindrome_substring.java
+++ strings/longest_palindrome_substring.java
@@ -2,27 +2,31 @@ package strings;
 
 public class longest_palindrome_substring {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() == 0) {
-            return "";
+        if(s.length()<=1){
+            return s;
         }
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i); // Odd length palindrome
-            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindrome
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
-            }
+        String maxStr = s.substring(0,1);
+        for (int i =0;i<s.length();i++){
+            String odd = expand(s,i,i);
+            String even = expand(s,i,i+1);
+            if(odd.length() > maxStr.length())
+            maxStr = odd;
+            if(even.length() > maxStr.length())
+            maxStr = even;
         }
-        return s.substring(start, end + 1);
-        }
-    private int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+        return maxStr;
+    }
+    private String expand(String s, int left,int right){
+        while(left>=0 && right< s.length() && s.charAt(left) == s.charAt(right)){
             left--;
             right++;
         }
-        return right - left - 1;
+        return s.substring(left+1,right);
     }
-    
+    public static void main(String[] args) {
+        longest_palindrome_substring obj = new longest_palindrome_substring();
+        String s = "babad";
+        String result = obj.longestPalindrome(s);
+        System.out.println("Longest Palindromic Substring: " + result);
+    }
 }
