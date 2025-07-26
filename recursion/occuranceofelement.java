e7a9b39afe653c76453c2dc833277a1ba94b2214
Omi Mishra <work.omimishra@gmail.com>
2025-07-26 22:37:35 +0530

    Add detailed comments to clarify the functionality of the occuranceofelement class

--- recursion/occuranceofelement.java
+++ recursion/occuranceofelement.java
@@ -1,7 +1,10 @@
 public class occuranceofelement {
     public static int first = -1;
     public static int last = -1;
-
+// This code finds the first and last occurrence of a character in a string using recursion.
+// It initializes two static variables, first and last, to -1. The findoccurance method is called with the string, an index starting at 0, and the character to search for.
+// The method checks each character in the string recursively, updating first and last when the character is found.
+// Finally, it prints the first and last occurrences of the character.
     public static void findoccurance(String str, int ind, char element) {
         if (ind == str.length()) {
             System.out.println(first);
