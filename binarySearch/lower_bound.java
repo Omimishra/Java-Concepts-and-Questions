e3c484f0bdb00125b7981b58fec1ca9ccb86efdd
Omi Mishra <work.omimishra@gmail.com>
2025-07-28 19:36:48 +0530

    #225:Implement lower bound binary search algorithm

---
+++ binarySearch/lower_bound.java
@@ -0,0 +1,26 @@
+package binarySearch;
+public class lower_bound {
+    public static int lowerBound(int []arr, int n, int x) {
+        int low = 0; int high = n-1;
+        int ans = n;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]>=x){
+            ans = mid;
+            high = mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 4, 5, 6};
+        int x = 4;
+        int n = arr.length;
+        int index = lowerBound(arr, n, x);
+        System.out.println("The lower bound of " + x + " is at index: " + index);
+    }
+}
