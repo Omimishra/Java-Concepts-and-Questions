438aecaf5e5351c2ce5d804c01af3a943b0150f0
Omi Mishra <work.omimishra@gmail.com>
2025-09-01 17:50:52 +0530

    #243 Implement kth missing element finder

---
+++ binarySearch/on_ans/kth_missing.java
@@ -0,0 +1,25 @@
+package binarySearch.on_ans;
+public class kth_missing {
+    public static int missing(int[] arr, int k){
+        int n = arr.length;
+       int low = 0;
+       int high = n-1;
+       while(low<=high){
+        int mid = (low+high)/2;
+        int missing = arr[mid] - (mid+1);
+        if(missing < k){
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+       }
+       return high + k + 1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,3,4,7,11};
+        int k = 5;
+        int result = missing(arr,k);
+        System.out.println("The kth missing element is: " + result);
+    }
+}
